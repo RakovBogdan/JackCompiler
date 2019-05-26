aae4014b7dc859bc01658d51fca62ca6106a6d97
RakovBogdan <rakovbogdan@gmail.com>
2019-05-27 01:14:25 +0300

    JackTokenizer work in progress

--- src/main/java/org/bohdanrakov/jackcompiler/JackTokenizer.java
+++ src/main/java/org/bohdanrakov/jackcompiler/JackTokenizer.java
@@ -1,12 +1,28 @@
 package org.bohdanrakov.jackcompiler;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Iterator;
 import java.util.List;
 
 public class JackTokenizer {
 
-    private List<String> lines;
+    public static final String NO_MORE_TOKENS = "There are no more tokens";
+    private List<String> tokens;
+    private Iterator<String> tokensIterator;
 
     public JackTokenizer(List<String> lines) {
-        this.lines = lines;
+    }
+
+    public void advance() {
+        if (hasMoreTokens()) {
+            tokensIterator.next();
+        } else {
+            throw new RuntimeException(NO_MORE_TOKENS);
+        }
+    }
+
+    private boolean hasMoreTokens() {
+        return tokensIterator.hasNext();
     }
 }
--- src/test/java/org/bohdanrakov/jackcompiler/JackTokenizerTest.java
+++ src/test/java/org/bohdanrakov/jackcompiler/JackTokenizerTest.java
@@ -1,6 +1,8 @@
 package org.bohdanrakov.jackcompiler;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
@@ -8,4 +10,6 @@ import static org.junit.Assert.*;
 @RunWith(MockitoJUnitRunner.class)
 public class JackTokenizerTest {
 
+    @InjectMocks
+    JackTokenizer testInstance;
 }
