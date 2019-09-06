61626b44a432d10dc09e486f3b30889bc615bd92
RakovBogdan <rakovbogdan@gmail.com>
2019-09-06 18:22:06 +0300

    compilation engine refactoring plus classVariableDeclaration implementation

--- src/main/java/org/bohdanrakov/jackcompiler/CompilationEngine.java
+++ src/main/java/org/bohdanrakov/jackcompiler/CompilationEngine.java
@@ -4,7 +4,7 @@ import org.bohdanrakov.jackcompiler.tokens.Token;
 import org.bohdanrakov.jackcompiler.tokens.TokenType;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -13,19 +13,28 @@ import static org.bohdanrakov.jackcompiler.tokens.TokenType.*;
 
 public class CompilationEngine {
 
-    private static Set<String> subroutinesTypes = Set.of("function", "constructor", "method");
+    private static List<String> subroutinesTypes = List.of("function", "constructor", "method");
     private List<Token> tokens;
     private List<String> result;
     private int tokenNumber;
 
     public List<String> compileClass(List<Token> tokens) {
-        tokenNumber = 0;
-        this.tokens = tokens;
-        result = new ArrayList<>();
+        initializeCompilingFromStart(tokens);
+
         result.add("<class>");
-        result.add("<keyword> " + getSpecifiedValue(KEYWORD, "class") + " </keyword>");
-        result.add("<identifier> " + getNextTokenStringValueByType(IDENTIFIER) + " </identifier>");
-        result.add("<symbol> " + getSpecifiedValue(SYMBOL, "{") + " </symbol>");
+        Token classToken = getNextToken();
+        checkTokenType(classToken.getTokenType(), KEYWORD);
+        checkTokenValue(classToken.getStringValue(), "class");
+        result.add("<keyword> " + classToken.getStringValue() + " </keyword>");
+
+        Token identifierToken = getNextToken();
+        checkTokenType(identifierToken.getTokenType(), IDENTIFIER);
+        result.add("<identifier> " + identifierToken.getStringValue() + " </identifier>");
+
+        Token openCurlyBraceToken = getNextToken();
+        checkTokenType(openCurlyBraceToken.getTokenType(), SYMBOL);
+        checkTokenValue(openCurlyBraceToken.getStringValue(), "{");
+        result.add("<symbol> " + openCurlyBraceToken.getStringValue() + " </symbol>");
 
         Token nextToken;
         while (!(nextToken = getNextToken()).getStringValue().equals("}")) {
@@ -42,12 +51,51 @@ public class CompilationEngine {
         return result;
     }
 
-    private void compileClassVariableDeclaration() {
+    private void initializeCompilingFromStart(List<Token> tokens) {
+        tokenNumber = 0;
+        this.tokens = tokens;
+        result = new ArrayList<>();
+    }
 
+    private void compileClassVariableDeclaration() {
+        result.add("<classVarDec>");
+
+        final String classVariableTokenValue = getCurrentToken().getStringValue();
+        checkTokenValue(classVariableTokenValue, "static", "field");
+        result.add("<keyword> " + classVariableTokenValue + " </keyword>");
+
+        Token variableTypeToken = getNextToken();
+        if (Set.of("int", "char", "boolean").contains(variableTypeToken.getStringValue())) {
+            result.add("<keyword> " + variableTypeToken.getStringValue() + " </keyword>");
+        } else {
+            checkTokenType(variableTypeToken.getTokenType(), IDENTIFIER);
+            result.add("<identifier> " + variableTypeToken.getStringValue() + " </identifier>");
+        }
+
+        Token variableNameToken = getNextToken();
+        checkTokenType(variableNameToken.getTokenType(), IDENTIFIER);
+        result.add("<identifier> " + variableNameToken.getStringValue() + " </identifier>");
+
+        while (getNextToken().getStringValue().equals(",")) {
+            result.add("<symbol> " + getCurrentToken().getStringValue() + " </symbol>");
+            variableNameToken = getNextToken();
+            checkTokenType(variableNameToken.getTokenType(), IDENTIFIER);
+            result.add("<identifier> " + variableNameToken.getStringValue() + " </identifier>");
+        }
+
+        Token endOfClassVariableDeclaration = getNextToken();
+        checkTokenValue(endOfClassVariableDeclaration.getStringValue(), ";");
+        result.add("<symbol> " + endOfClassVariableDeclaration.getStringValue() + " </symbol>");
+
+        result.add("</classVarDec>");
     }
 
     private void compileSubroutineDeclaration() {
 
+    }
+
+    private Token getCurrentToken() {
+        return tokens.get(tokenNumber);
     }
 
     private Token getNextToken() {
@@ -58,32 +106,26 @@ public class CompilationEngine {
         }
     }
 
-    private String getSpecifiedValue(TokenType tokenType, String expectedValue) {
-        String nextTokenStringValue = getNextTokenStringValueByType(tokenType);
-        checkTokenValue(nextTokenStringValue, expectedValue);
-        return nextTokenStringValue;
-    }
-
-    private void checkTokenValue(String tokenValue, String expectedValue) {
-        if (!expectedValue.equals(tokenValue)) {
-            throw new RuntimeException("Wrong Value " + tokenValue + " was Passed. Expected " + expectedValue);
+    private void checkTokenValue(String currentTokenValue, String... allowedTokenValues) {
+        for (String expectedValue : allowedTokenValues) {
+            if (expectedValue.equals(currentTokenValue)) {
+                return;
+            }
         }
+        throw new RuntimeException("The source code token value:" + currentTokenValue +
+                " doesn't match allowed token types:" + Arrays.toString(allowedTokenValues));
     }
 
-    private String getNextTokenStringValueByType(TokenType tokenType) {
-        final Token nextToken = checkNextTokenByType(tokenType);
-        return nextToken.getStringValue();
-    }
-
-    private Token checkNextTokenByType(TokenType tokenType) {
-        final Token nextToken = getNextToken();
-        if (nextToken.getTokenType() != tokenType) {
-            throw new RuntimeException("The expected token type " + tokenType +
-                    "is not same as in source code " + nextToken.getTokenType());
+    private void checkTokenType(TokenType currentTokenType, TokenType... allowedTokenTypes) {
+        for (TokenType allowedTokenType : allowedTokenTypes) {
+            if (currentTokenType == allowedTokenType) {
+                return;
+            }
         }
-
-        return nextToken;
+        throw new RuntimeException("The source code token type:" + currentTokenType +
+                " doesn't match allowed token types:" + Arrays.toString(allowedTokenTypes));
     }
+
 
     public List<String> compileClassWithoutAnalyzing(List<Token> tokens) {
         List<String> result = new ArrayList<>();
